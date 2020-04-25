52bad2a932aacafc779ca64de897c7088c50420b
Esten Rye <esten@ryezone.net>
2020-04-24 22:45:19 -0500

    Fix web entrypoint logic.

--- deploy.ps1
+++ deploy.ps1
@@ -7,17 +7,19 @@ param(
 )
 $env:PWD = ${PSScriptRoot}.Replace('\', '/')
 $env:TOP_LEVEL_DOMAIN = 'localhost'
+$env:TRAEFIK_WEB_ENTRYPOINT = 'web'
 $env:LETS_ENCRYPT_EMAIL = $LetsEncryptEmail
 $baseFile = "$env:PWD/stack.yml"
 $overrideFile = "$env:PWD/stack.development.yml"
 if ($IsProduction) {
     $env:TOP_LEVEL_DOMAIN = $TopLevelDomain
+    $env:TRAEFIK_WEB_ENTRYPOINT = 'websecure'
     $overrideFile = "$env:PWD/stack.production.yml"
 }
+
 $env:PWD = $env:PWD.Replace('C:/', '/c/')
 if ($Deploy)
 {
-    $env:TRAEFIK_WEB_ENTRYPOINT = 'websecure'
     docker stack deploy -c "$baseFile" -c "$overrideFile" seis635
 }
 else
