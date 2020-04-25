6f3bfcb4879241b89ec07fee729fb226824460d9
Esten Rye <esten@ryezone.net>
2020-04-25 14:02:38 -0500

    Move elastic search into deploy.ps1 stack.

--- deploy.ps1
+++ deploy.ps1
@@ -9,9 +9,12 @@ $env:PWD = ${PSScriptRoot}.Replace('\', '/')
 $env:TOP_LEVEL_DOMAIN = 'localhost'
 $env:TRAEFIK_WEB_ENTRYPOINT = 'web'
 $env:LETS_ENCRYPT_EMAIL = $LetsEncryptEmail
+
 $baseFile = "${PSScriptRoot}/stacks/stack.yml"
 $overrideFile = "${PSScriptRoot}/stacks/stack.development.yml"
 $mongoFile = "${PSScriptRoot}/stacks/stack.mongo.yml"
+$elasticSearchFile = "${PSScriptRoot}/stacks/stack.elasticsearch.yml"
+
 if ($IsProduction) {
     $env:TOP_LEVEL_DOMAIN = $TopLevelDomain
     $env:TRAEFIK_WEB_ENTRYPOINT = 'websecure'
@@ -21,11 +24,11 @@ if ($IsProduction) {
 $env:PWD = $env:PWD.Replace('C:/', '/c/')
 if ($Deploy)
 {
-    docker stack deploy -c ${baseFile} -c ${overrideFile} -c ${mongoFile} seis635
+    docker stack deploy -c ${baseFile} -c ${overrideFile} -c ${mongoFile} -c ${elasticSearchFile} seis635
 }
 else
 {
     $baseFile
     $overrideFile
-    docker-compose -f $baseFile -f $overrideFile -f ${mongoFile} config
+    docker-compose -f $baseFile -f $overrideFile -f ${mongoFile} -f ${elasticSearchFile} config
 }
